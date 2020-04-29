681d345b3f1db991f852b14aeaffba28b170b017
Isaac Pugh <ipugh@xmatters.com>
2020-04-29 12:16:10 -0400

    gcloud run and docker

--- main.go
+++ main.go
@@ -2,19 +2,28 @@ package main
 
 import (
     "github.com/gin-gonic/gin"
+    "time"
+    "math/rand"
 )
 
 func main() {
 	r := gin.Default()
+
 	r.GET("/eat", func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.JSON(200, gin.H{
 			"message": "you ate food",
 		})
 	})
+
 	r.GET("/sleep", func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        length := rand.Intn(20)
+        time.Sleep(time.Duration(length + 20) * time.Second)
 		c.JSON(200, gin.H{
 			"message": "you slept",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+    r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
