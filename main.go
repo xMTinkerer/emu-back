585d35a1735a61411486229fbaeb98e14dfe48a6
tdepuy@xmatters.com <tdepuy@xmatters.com>
2020-07-14 15:12:54 -0700

    noemu

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 
         c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.JSON(200, gin.H{
-			"result": "/img/HatOn.png", 
+			"result": "/img/noemu.png", 
         })    
 
     })
